ee5bf203c91eeee3e7b4eeccc5642e993f1652ab
mochalinEA <com-mail@mail.ru>
2018-11-22 01:46:42 +0300

    Added seats prototype

---
+++ seats/mocks/api_response.js
@@ -0,0 +1,531 @@
+window.response = {
+  seats: [
+    // group1
+    {
+      id: 'f1',
+      isBusy: true,
+    },
+    {
+      id: 'f2',
+      isBusy: true,
+    },
+
+    {
+      id: 'd1',
+      isBusy: true,
+    },
+    {
+      id: 'd2',
+      isBusy: true,
+    },
+
+
+    // group 2
+    {
+      id: 'f3',
+      isBusy: true,
+    },
+    {
+      id: 'f4',
+      isBusy: false,
+    },
+    {
+      id: 'f5',
+      isBusy: true,
+    },
+    {
+      id: 'f6',
+      isBusy: true,
+    },
+    {
+      id: 'f7',
+      isBusy: true,
+    },
+    {
+      id: 'f8',
+      isBusy: true,
+    },
+    {
+      id: 'd3',
+      isBusy: true,
+    },
+    {
+      id: 'd4',
+      isBusy: false,
+    },
+    {
+      id: 'd5',
+      isBusy: true,
+    },
+    {
+      id: 'd6',
+      isBusy: true,
+    },
+    {
+      id: 'd7',
+      isBusy: true,
+    },
+    {
+      id: 'd8',
+      isBusy: true,
+    },
+    {
+      id: 'e3',
+      isBusy: true,
+    },
+    {
+      id: 'e4',
+      isBusy: false,
+    },
+    {
+      id: 'e5',
+      isBusy: true,
+    },
+    {
+      id: 'e6',
+      isBusy: true,
+    },
+    {
+      id: 'e7',
+      isBusy: true,
+    },
+    {
+      id: 'e8',
+      isBusy: true,
+    },
+
+
+    // group 3
+    {
+      id: 'e9',
+      isBusy: true,
+    },
+    {
+      id: 'e10',
+      isBusy: false,
+    },
+    {
+      id: 'e11',
+      isBusy: true,
+    },
+    {
+      id: 'e12',
+      isBusy: true,
+    },
+    {
+      id: 'e13',
+      isBusy: true,
+    },
+    {
+      id: 'e14',
+      isBusy: true,
+    },
+    {
+      id: 'e15',
+      isBusy: true,
+    },
+    {
+      id: 'e16',
+      isBusy: false,
+    },
+    {
+      id: 'e17',
+      isBusy: true,
+    },
+    {
+      id: 'e18',
+      isBusy: true,
+    },
+    {
+      id: 'e19',
+      isBusy: true,
+    },
+    {
+      id: 'e20',
+      isBusy: true,
+    },
+    {
+      id: 'f9',
+      isBusy: true,
+    },
+    {
+      id: 'f10',
+      isBusy: false,
+    },
+    {
+      id: 'f11',
+      isBusy: true,
+    },
+    {
+      id: 'f12',
+      isBusy: true,
+    },
+    {
+      id: 'f13',
+      isBusy: true,
+    },
+    {
+      id: 'f14',
+      isBusy: true,
+    },
+    {
+      id: 'f15',
+      isBusy: true,
+    },
+    {
+      id: 'f16',
+      isBusy: false,
+    },
+    {
+      id: 'f17',
+      isBusy: true,
+    },
+    {
+      id: 'f18',
+      isBusy: true,
+    },
+    {
+      id: 'f19',
+      isBusy: true,
+    },
+    {
+      id: 'f20',
+      isBusy: true,
+    },
+    {
+      id: 'd9',
+      isBusy: true,
+    },
+    {
+      id: 'd10',
+      isBusy: false,
+    },
+    {
+      id: 'd11',
+      isBusy: true,
+    },
+    {
+      id: 'd12',
+      isBusy: true,
+    },
+    {
+      id: 'd13',
+      isBusy: true,
+    },
+    {
+      id: 'd14',
+      isBusy: true,
+    },
+    {
+      id: 'd15',
+      isBusy: true,
+    },
+    {
+      id: 'd16',
+      isBusy: false,
+    },
+    {
+      id: 'd17',
+      isBusy: true,
+    },
+    {
+      id: 'd18',
+      isBusy: true,
+    },
+    {
+      id: 'd19',
+      isBusy: true,
+    },
+    {
+      id: 'd20',
+      isBusy: true,
+    },
+
+
+    // group 4
+    {
+      id: 'c1',
+      isBusy: true,
+    },
+    {
+      id: 'c2',
+      isBusy: true,
+    },
+    {
+      id: 'a1',
+      isBusy: true,
+    },
+    {
+      id: 'a2',
+      isBusy: true,
+    },
+
+
+    // group 5
+    {
+      id: 'c3',
+      isBusy: false,
+    },
+    {
+      id: 'c4',
+      isBusy: false,
+    },
+    {
+      id: 'c5',
+      isBusy: false,
+    },
+    {
+      id: 'c6',
+      isBusy: false,
+    },
+    {
+      id: 'c7',
+      isBusy: false,
+    },
+    {
+      id: 'c8',
+      isBusy: false,
+    },
+    {
+      id: 'b3',
+      isBusy: true,
+    },
+    {
+      id: 'b4',
+      isBusy: false,
+    },
+    {
+      id: 'b5',
+      isBusy: true,
+    },
+    {
+      id: 'b6',
+      isBusy: true,
+    },
+    {
+      id: 'b7',
+      isBusy: true,
+    },
+    {
+      id: 'b8',
+      isBusy: true,
+    },
+    {
+      id: 'a3',
+      isBusy: true,
+    },
+    {
+      id: 'a4',
+      isBusy: false,
+    },
+    {
+      id: 'a5',
+      isBusy: true,
+    },
+    {
+      id: 'a6',
+      isBusy: true,
+    },
+    {
+      id: 'a7',
+      isBusy: true,
+    },
+    {
+      id: 'a8',
+      isBusy: true,
+    },
+
+    // group 6
+    {
+      id: 'a9',
+      isBusy: true,
+    },
+    {
+      id: 'a10',
+      isBusy: false,
+    },
+    {
+      id: 'a11',
+      isBusy: true,
+    },
+    {
+      id: 'a12',
+      isBusy: true,
+    },
+    {
+      id: 'a13',
+      isBusy: true,
+    },
+    {
+      id: 'a14',
+      isBusy: true,
+    },
+    {
+      id: 'a15',
+      isBusy: true,
+    },
+    {
+      id: 'a16',
+      isBusy: false,
+    },
+    {
+      id: 'a17',
+      isBusy: true,
+    },
+    {
+      id: 'a18',
+      isBusy: true,
+    },
+    {
+      id: 'a19',
+      isBusy: true,
+    },
+    {
+      id: 'a20',
+      isBusy: true,
+    },
+    {
+      id: 'c9',
+      isBusy: true,
+    },
+    {
+      id: 'c10',
+      isBusy: false,
+    },
+    {
+      id: 'c11',
+      isBusy: true,
+    },
+    {
+      id: 'c12',
+      isBusy: true,
+    },
+    {
+      id: 'c13',
+      isBusy: true,
+    },
+    {
+      id: 'c14',
+      isBusy: true,
+    },
+    {
+      id: 'c15',
+      isBusy: true,
+    },
+    {
+      id: 'c16',
+      isBusy: false,
+    },
+    {
+      id: 'c17',
+      isBusy: true,
+    },
+    {
+      id: 'c18',
+      isBusy: true,
+    },
+    {
+      id: 'c19',
+      isBusy: true,
+    },
+    {
+      id: 'c20',
+      isBusy: true,
+    },
+    {
+      id: 'b9',
+      isBusy: true,
+    },
+    {
+      id: 'b10',
+      isBusy: false,
+    },
+    {
+      id: 'b11',
+      isBusy: true,
+    },
+    {
+      id: 'b12',
+      isBusy: true,
+    },
+    {
+      id: 'b13',
+      isBusy: true,
+    },
+    {
+      id: 'b14',
+      isBusy: true,
+    },
+    {
+      id: 'b15',
+      isBusy: true,
+    },
+    {
+      id: 'b16',
+      isBusy: false,
+    },
+    {
+      id: 'b17',
+      isBusy: true,
+    },
+    {
+      id: 'b18',
+      isBusy: true,
+    },
+    {
+      id: 'b19',
+      isBusy: true,
+    },
+    {
+      id: 'b20',
+      isBusy: true,
+    },
+  ],
+
+
+  groups: [
+    {
+      id: 1,
+      seatSize: 'big',
+      rowCount: 2,
+      columnCount: 2,
+      seatIds: ['f1', 'f2', 'd1', 'd2'],
+    },
+    {
+      id: 2,
+      seatSize: 'small',
+      rowCount: 3,
+      columnCount: 6,
+      seatIds: ['f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8'],
+    },
+    {
+      id: 3,
+      seatSize: 'small',
+      rowCount: 3,
+      columnCount: 12,
+      seatIds: ['f9', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f20', 'e9', 'e10', 'e11', 'e12', 'e13', 'e14', 'e15', 'e16', 'e17', 'e18', 'e19', 'e20', 'd9', 'd10', 'd11', 'd12', 'd13', 'd14', 'd15', 'd16', 'd17', 'd18', 'd19', 'd20'],
+    },
+
+    {
+      id: 4,
+      seatSize: 'big',
+      rowCount: 2,
+      columnCount: 2,
+      seatIds: ['c1', 'c2', 'a1', 'a2'],
+    },
+    {
+      id: 5,
+      seatSize: 'small',
+      rowCount: 3,
+      columnCount: 6,
+      seatIds: ['c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8'],
+    },
+    {
+      id: 6,
+      seatSize: 'small',
+      rowCount: 3,
+      columnCount: 12,
+      seatIds: ['c9', 'c10', 'c11', 'c12', 'c13', 'c14', 'c15', 'c16', 'c17', 'c18', 'c19', 'c20', 'a9', 'a10', 'a11', 'a12', 'a13', 'a14', 'a15', 'a16', 'a17', 'a18', 'a19', 'a20', 'b9', 'b10', 'b11', 'b12', 'b13', 'b14', 'b15', 'b16', 'b17', 'b18', 'b19', 'b20'],
+    },
+  ]
+};
